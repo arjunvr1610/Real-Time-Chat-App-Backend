9757627d6fbbae5cf4c3a8f766176b8fc9e4a9c9
Arjun V R <72267684+arjunvr1610@users.noreply.github.com>
2022-06-20 20:49:16 +0530

    third commit

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const mongoURI = `mongodb+srv://monkey-chat:J3T7As1la2AYXxax@cluster0.f7cvi.mongodb.net/ChatApp?retryWrites=true&w=majority`;
 
 const mongoURI = "mongodb://localhost:27017/ChatApp"
 
